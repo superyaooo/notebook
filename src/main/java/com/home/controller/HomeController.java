fe2d5b338f8992449b5dbc7411e61a05672cdd84
superyaooo <superyaooo@gmail.com>
2017-11-05 23:58:37 -0600

    add thymeleaf dependency

--- src/main/java/com/home/controller/HomeController.java
+++ src/main/java/com/home/controller/HomeController.java
@@ -1,12 +1,12 @@
 package com.home.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 	@RequestMapping("/")
 	public String home() {
-		return "Hey, this is my very 1st Spring Boot app!";
+		return "index";
 	}
 }
